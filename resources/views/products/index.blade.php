de262d6273f3662e630f869f331260a049a3293c
EC2 Default User <ec2-user@ip-172-31-26-126.ap-northeast-1.compute.internal>
2021-11-06 05:22:21 +0000

    05 create login system

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="products-container">
+    <div class="products-home">商品管理</div>
+    <div class="maintenance">
+        商品メンテナンス
+    </div>
+    <div class="maker">
+        メーカー
+    </div>
+    <div class="genre">
+        ジャンル
+    </div>
+    <div class="category">
+        カテゴリー
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/orders', [OrderController::class, 'index'])->name('orders');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
