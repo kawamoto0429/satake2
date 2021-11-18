48b6a6df4137f2c97ce2fa2a78b0749e6f214868
EC2 Default User <ec2-user@ip-172-31-26-126.ap-northeast-1.compute.internal>
2021-11-18 14:31:59 +0000

    create_fail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <!--<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>-->
 
                             <div class="col-md-6">
-                                <input id="email" type="hidden" class="form-control @error('email') is-invalid @enderror" name="email" value="test19@gmail.com" required autocomplete="email" autofocus>
+                                <input id="email" type="test" class="form-control @error('email') is-invalid @enderror" name="email" value="test2@gmail.com" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -38,19 +38,19 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Comfirm_Password') }}</label>
+                        <!--<div class="form-group row">-->
+                        <!--    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Comfirm_Password') }}</label>-->
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <!--    <div class="col-md-6">-->
+                        <!--        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">-->
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                        <!--        @error('password')-->
+                        <!--            <span class="invalid-feedback" role="alert">-->
+                        <!--                <strong>{{ $message }}</strong>-->
+                        <!--            </span>-->
+                        <!--        @enderror-->
+                        <!--    </div>-->
+                        <!--</div>-->
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
