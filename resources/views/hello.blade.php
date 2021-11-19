488ab19693f804715345060a2ff2946e2e617946
EC2 Default User <ec2-user@ip-172-31-26-126.ap-northeast-1.compute.internal>
2021-11-19 07:46:38 +0000

    create_pdf

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>PDF</title>
+<style>
+@font-face{
+    font-family: ipag;
+    font-style: normal;
+    font-weight: normal;
+    src:url('{{ storage_path('fonts/ipag.ttf')}}');
+}
+body {
+font-family: ipag;
+}
+</style>
+</head>
+<body>
+    <p>ヤマザキ</p>
+</div>
+</body>
+</html>
