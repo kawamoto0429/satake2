5d21b08d2841ccfa9acc40667dc125b6266592d5
EC2 Default User <ec2-user@ip-172-31-26-126.ap-northeast-1.compute.internal>
2021-12-21 10:56:15 +0000

    main

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -5,14 +5,14 @@
 
 <div class="orders-container w-600px m-auto ">
     <div class="orders-top mb-2 ">
-        <a class="btn btn-primary btn-lg w-100 h-80px" href="/">
+        <a class="btn btn-primary btn-lg w-100 h-80px p-auto" href="/">
             メーカーを選んでください
         </a>
     </div>
 
     @foreach($makers as $maker)
         <div class="maker mb-2">
-            <a class="btn btn-primary btn-lg w-100 h-80px" href="/orders/{{$maker->id}}/home">
+            <a class="btn btn-primary btn-lg w-100 h-80px p-auto" href="/orders/{{$maker->id}}/home">
                 {{$maker->name}}
             </a>
         </div>
